f3eeb0c2e310a06f29cd5766417fb1bc7830a8b0
Bella <2770706h@student.gla.ac.uk>
2024-11-17 19:00:07 +0000

    implemented Sanitizer

--- src/Sanitizer.py
+++ src/Sanitizer.py
@@ -1,5 +1,43 @@
+import string
+
+
+def _strip_special_chars(line: str):
+    token = line.strip("~`!@#$%^&*()_-+={}[]|:;'<>,.?/")
+    return token
+
+
+def _remove_non_ascii(line: str):
+    ascii_line: str = ""
+    for character in line:
+        if ord(character) < 128 or character == "." or character == " ":
+            ascii_line += character
+
+    return ascii_line
+
+
+def _strip_specials_from_words(list_of_words: list[str]):
+    '''
+    removes leading and trailing special characters from each string in the list
+    '''
+    sanitized_list = []
+    for word in list_of_words:
+        word = _strip_special_chars(word)
+        sanitized_list.append(word)
+
+    return sanitized_list
+
+
+def _remove_empty_words(list_of_words):
+    no_empties = []
+    for word in list_of_words:
+        if word != "":
+            no_empties.append(word)
+
+    return no_empties
+
+
 class Sanitizer:
-    def parse_line(self, line):
+    def parse_line(self, line: str):
         """
         Parses a given line,
         removes whitespaces, splits into list of sanitize words
@@ -8,7 +46,18 @@ class Sanitizer:
         HINT: Consider using the "strip()" and "split()" function here
 
         """
+        ascii_line: str = _remove_non_ascii(line)
 
-        # TODO
-        list_of_words = []
-        return list_of_words
+        lowercase_line: str = ascii_line.lower()
+
+        list_of_words: list[str] = lowercase_line.split()
+
+        stripped_list: list[str] = _strip_specials_from_words(list_of_words)
+
+        no_empty_words: list[str] = _remove_empty_words(stripped_list)
+
+        return no_empty_words
+
+
+if __name__ == "__main__":
+    print(string.printable)
---
+++ test/TestSanitizer.py
@@ -0,0 +1,32 @@
+import unittest
+from src.Sanitizer import Sanitizer
+
+
+class TestSanitizer(unittest.TestCase):
+    def setUp(self):
+        self.sanitizer = Sanitizer()
+
+    def test_removes_non_ascii(self):
+        actual_line = self.sanitizer.parse_line("日本人 中國的 ascii characters")
+        self.assertEqual(["ascii", "characters"], actual_line)
+
+    def test_removes_capitals(self):
+        actual_line = self.sanitizer.parse_line("I AM very ANGRY")
+        self.assertEqual(["i", "am", "very", "angry"], actual_line)
+
+    def test_removes_whitespace(self):
+        actual_line = self.sanitizer.parse_line("    i   declare the  eeee wo hooo       a      - to    ")
+        self.assertEqual(["i", "declare", "the", "eeee", "wo", "hooo", "a", "to"], actual_line)
+
+    def test_removes_only_leading_trailing_special_chars(self):
+        actual_line = self.sanitizer.parse_line(
+            "~`!@#$%^&*()_-+={}[]|:;'<>,.?/ ..._&*()__...anna_kerenina.txt...___()_-+={}[]|:;'<>,.?  )_-+=")
+        self.assertEqual(["anna_kerenina.txt"], actual_line)
+
+    def test_returns_list(self):
+        actual_line = self.sanitizer.parse_line("hi")
+        self.assertEqual(["hi"], actual_line)
+
+
+if __name__ == '__main__':
+    unittest.main()
