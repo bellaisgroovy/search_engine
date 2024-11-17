435f3781095659787ab103c0567ee85f6b520ea1
Bella <2770706h@student.gla.ac.uk>
2024-11-17 19:20:24 +0000

    added whitespace

--- src/IndexUpdater.py
+++ src/IndexUpdater.py
@@ -1,5 +1,5 @@
 class IndexUpdater:
-    def update_forward_index(self,forward_index: dict[str, set[str]], doc_name: str, word: str):
+    def update_forward_index(self, forward_index: dict[str, set[str]], doc_name: str, word: str):
         if forward_index.get(doc_name) is None:
             forward_index[doc_name] = {word}
         else:
