1fd9497176db2f5ee39b16d2db99bbd031a77da5
Bella <2770706h@student.gla.ac.uk>
2024-11-18 18:57:21 +0000

    optimized parameters, renamed test

--- src/Scale.py
+++ src/Scale.py
@@ -3,14 +3,14 @@ class Scale:
               search_phrase: list[str],
               doc_name: str,
               doc_rank: dict[str, float],
-              term_freq: dict[str, dict[str, float]],
+              doc_term_freq: dict[str, float],
               inv_doc_freq: dict[str, float]):
 
         weight: float = doc_rank.get(doc_name)
 
         for word in search_phrase:
 
-            word_weight: float = term_freq.get(doc_name).get(word) * inv_doc_freq.get(word)
+            word_weight: float = doc_term_freq.get(word) * inv_doc_freq.get(word)
 
             weight = weight * word_weight
 
--- test/TestScale.py
+++ test/TestScale.py
@@ -5,12 +5,12 @@ from src.Scale import Scale
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.Scale = Scale()
-    def test_something(self):
+    def test_weigh(self):
         word = "hello"
         search_phrase: list[str] = [word]
         doc_name: str = "hi.txt"
         doc_rank: dict[str, float] = {doc_name: 1/100}  # there are 100 words in hi.txt
-        term_freq: dict[str, dict[str, float]] = {doc_name: {word: 5/100}}  # 5/100 words in hi.txt are hello
+        term_freq: dict[str, float] = {word: 5/100}  # 5/100 words in hi.txt are hello
         inv_doc_freq: dict[str, float] = {word: 1/2} # one of two documents say hello
 
         weight: float = self.Scale.weigh(search_phrase, doc_name, doc_rank, term_freq, inv_doc_freq)
